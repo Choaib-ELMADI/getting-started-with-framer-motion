e9262517fd09faca58de7f56bd680f1073d35b56
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-06-08 22:51:57 +0100

    Clean files and folders

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App.jsx';
+import './index.scss';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
