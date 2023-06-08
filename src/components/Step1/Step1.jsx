af0260961b5f0243e192485add233ea7f81aba03
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-06-09 00:52:09 +0100

    Add Step 2

--- src/components/Step1/Step1.jsx
+++ src/components/Step1/Step1.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import '../../App.scss';
-const places = ['Imi-Nwarg', 'Agnso-Nwarg', 'Tasli', 'Ait-Etiq', 'Tamda'];
+const places = ['Imi-Nwarg', 'Tasli', 'Ait-Etiq', 'Tamda'];
 
 
 
--- src/components/Step2/Step2.jsx
+++ src/components/Step2/Step2.jsx
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import '../../App.scss';
+const activities = ['Activity  01', 'Activity  02', 'Activity  03', 'Activity  04', 'Activity  05'];
 
 
 
 const Step2 = () => {
+    const [selectedActivities, setSelectedActivities] = useState([activities]);
+
+    const handleSelect = (activity) => {
+        if (selectedActivities.includes(activity)) {
+            setSelectedActivities(
+                selectedActivities.filter((act) => (
+                    act !== activity
+                ))
+            );
+        }
+        else {
+            setSelectedActivities(
+                [ ...selectedActivities, activity ]
+            );
+        }
+    };
+
     return (
         <div className='app__step'>
-            <Link className='link' to='/step3'>Visit Ait-mgoun</Link>
+            <h2>Step 2: Choose activities</h2>
+            <ul>
+                {
+                    activities.map((activity) => (
+                        <li
+                            key={ activity }
+                            onClick={ () => handleSelect(activity) }
+                            className={ selectedActivities.includes(activity) ? 'active' : '' }
+                        >
+                            { activity }
+                        </li>
+                    ))
+                }
+            </ul>
+            {
+                selectedActivities.length >= 1 &&
+                <Link className='link' to='/step3'>Visit Ait-mgoun</Link>
+            }
         </div>
     );
 };
